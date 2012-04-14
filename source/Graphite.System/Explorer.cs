bf49bd86bef75c2e1edf80f7292e8881cb6564af
peschuster <schuster.pe@gmail.com>
2012-04-14 18:08:22 +0200

    Added "-e" (explore PerformanceCounter names) and "-h"/"-?" (usage help) parameters.

---
+++ source/Graphite.System/Explorer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Graphite.System
+{
+    internal static class Explorer
+    {
+        public static void Print(string[] path)
+        {
+            string[] filteredPath = path == null
+                ? new string[0]
+                : path.Where(s => !string.IsNullOrWhiteSpace(s)).Select(s => UnQuote(s)).ToArray();
+
+            if (filteredPath.Length == 0)
+            {
+                Console.WriteLine("Categories:");
+
+                var categories = PerformanceCounterCategory.GetCategories();
+
+                string intend = new string(' ', 4);
+                foreach (var category in categories)
+                {
+                    Console.WriteLine(intend + category.CategoryName);
+                }
+            }
+            else
+            {
+                PerformanceCounterCategory category = FindCategory(filteredPath[0]);
+
+                if (category == null)
+                {
+                    Console.WriteLine("Unkown category: " + filteredPath[0]);
+                }
+                else
+                {
+                    Console.WriteLine("Category: " + category.CategoryName);
+
+                    if (filteredPath.Length == 1)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Instances:");
+                        string intend = new string(' ', 4);
+                        foreach (var instance in category.GetInstanceNames())
+                        {
+                            Console.WriteLine(intend + instance);
+                        }
+                    }
+                    else
+                    {
+                        if (!category.InstanceExists(filteredPath[1]))
+                        {
+                            Console.WriteLine("Unkown instance: " + filteredPath[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Instance: " + filteredPath[1]);
+                            Console.WriteLine();
+                            Console.WriteLine("Counters:");
+                            string intend = new string(' ', 4);
+                            foreach (var counter in category.GetCounters(filteredPath[1]))
+                            {
+                                Console.WriteLine(intend + counter.CounterName);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string UnQuote(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return value;
+
+            if ((value.StartsWith("\"") && value.EndsWith("\""))
+                || (value.StartsWith("'") && value.EndsWith("'")))
+            {
+                return value.Substring(1, value.Length - 2);
+            }
+
+            return value;
+        }
+
+        private static PerformanceCounterCategory FindCategory(string name)
+        {
+            try
+            {
+                return new PerformanceCounterCategory(name);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+        }
+    }
+}
--- source/Graphite.System/Program.cs
+++ source/Graphite.System/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.ServiceProcess;
 using Graphite.Configuration;
 using Graphite.System.Configuration;
@@ -7,18 +8,42 @@ namespace Graphite.System
 {
     public static class Program
     {
-        public static void Main()
+        public static void Main(params string[] parameter)
         {
             if (Environment.UserInteractive)
             {
                 // Start as console...
+                Func<string, bool> isParamater = (s) => s != null && (s.StartsWith("-") || s.StartsWith("/"));
+                Func<string, bool> isEParamater = (s) => s == "-e" || s == "/e";
+                Func<string, bool> isHParamater = (s) => s == "-h" || s == "/h" || s == "-?" || s == "/?" || s == "--help" || s == "/help";
 
-                using (new Kernel(GraphiteConfiguration.Instance, GraphiteSystemConfiguration.Instance))
+                if (parameter != null && parameter.Any(isHParamater))
                 {
-                    Console.WriteLine("Monitoring configured performance counters.");
-                    Console.WriteLine("Press [enter] to exit...");
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine();
+                    Console.WriteLine("[no parameters] -> Start listening on configured PerformanceCounters (App.config)");
+                    Console.WriteLine("-e [category] [instance] -> Explore PerformanceCounters (all or by category or by category and instance)");
+                    Console.WriteLine();
+                }
+                else if (parameter != null && parameter.Any(isEParamater))
+                {
+                    string[] path = parameter
+                        .SkipWhile(s => !isEParamater(s))
+                        .Skip(1)
+                        .TakeWhile(s => !isParamater(s))
+                        .ToArray();
 
-                    Console.ReadLine();
+                    Explorer.Print(path);
+                }
+                else
+                {
+                    using (new Kernel(GraphiteConfiguration.Instance, GraphiteSystemConfiguration.Instance))
+                    {
+                        Console.WriteLine("Monitoring configured performance counters.");
+                        Console.WriteLine("Press [enter] to exit...");
+
+                        Console.ReadLine();
+                    }
                 }
             }
             else
