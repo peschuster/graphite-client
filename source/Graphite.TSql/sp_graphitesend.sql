875dd4224fbda7c66b9ee901120b9a60e018e257
Peter Schuster <schuster.pe@gmail.com>
2012-07-08 23:43:27 +0200

    Added sql for sp_graphitesend creation.

---
+++ source/Graphite.TSql/sp_graphitesend.sql
@@ -0,0 +1,32 @@
+USE [master]
+GO
+
+-- enable 'TRUSTWORTHY'
+-- required for 'external access' of clr code (-> sending tcp packets).
+ALTER DATABASE [master] SET TRUSTWORTHY ON;
+GO
+
+-- Add Graphite.TSql.dll assembly
+CREATE ASSEMBLY [Graphite.TSql]
+AUTHORIZATION [dbo]
+FROM '<your/path/to/Graphite.TSql.dll>'
+WITH PERMISSION_SET = EXTERNAL_ACCESS
+GO
+
+-- Create stored procedure 'sp_graphitesend'
+CREATE PROCEDURE sp_graphitesend
+(
+@host nvarchar(255),
+@port int,
+@key nvarchar(255)
+)
+AS
+EXTERNAL NAME [Graphite.TSql].[Graphite.TSql.GraphiteProcedures].GraphiteSend
+GO
+
+-- --------------------------------------------------------------------------
+-- Example usage:
+--
+-- exec sp_graphitesend N'192.168.0.1', 2003, 'stats.events.myserver.test'
+--
+-- --------------------------------------------------------------------------
