ce835e716b4a0f21708117c8f510d8e716c83ca0
Peter Schuster <schuster.pe@gmail.com>
2012-09-04 15:11:46 +0200

    Added basic reporting of elmah errors

---
+++ source/Graphite.Elmah/AssertionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml;
+using Elmah.Assertions;
+
+namespace Graphite
+{
+    public sealed class AssertionFactory
+    {
+        public static IAssertion statsd_log(XmlElement config)
+        {
+            return new StatsDLogAssertion();
+        }
+    }
+}
---
+++ source/Graphite.Elmah/StatsDLogAssertion.cs
@@ -0,0 +1,48 @@
+﻿using System.Configuration;
+using System.Linq;
+using Elmah.Assertions;
+using Graphite.Configuration;
+using Graphite.Infrastructure;
+
+namespace Graphite
+{
+    public class StatsDLogAssertion : IAssertion
+    {
+        private readonly ChannelFactory factory;
+
+        private readonly IMonitoringChannel channel;
+
+        private readonly string metricKey;
+
+        private const string AppSettingsKey = "elmahStatsDKey";
+
+        private const string DefaultMetricKey = "elmah_error";
+
+        public StatsDLogAssertion()
+        {
+            this.factory = new ChannelFactory(
+                null, 
+                GraphiteConfiguration.Instance.StatsD);
+
+            if (ConfigurationManager.AppSettings.AllKeys.Contains(AppSettingsKey))
+            {
+                this.metricKey = ConfigurationManager.AppSettings[AppSettingsKey];
+            }
+
+            if (string.IsNullOrEmpty(this.metricKey))
+            {
+                this.metricKey = DefaultMetricKey;
+            }
+
+            this.channel = this.factory.CreateChannel("counter", "statsd");
+        }
+
+        public bool Test(object context)
+        {
+            this.channel.Report(this.metricKey, 1);
+
+            // Do not filter this error.
+            return false;
+        }
+    }
+}
