4a0fe0851dbca51b0451c8be83f3f081c034c410
Peter Schuster <schuster.pe@gmail.com>
2014-01-05 13:56:05 +0100

    Fix build for build servers
    Excludes mvc test projects for release builds and activates nuget package restore.

--- build/go.bat
+++ build/go.bat
@@ -6,6 +6,8 @@ SET msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
 IF '%1'=='' (SET target="Build") ELSE (SET target=%1)
 IF '%2'=='' (SET buildConfig="Release") ELSE (SET buildConfig=%2)
 
+SET EnableNuGetPackageRestore=true
+
 %msbuild% .\build.proj /t:%target% /property:Configuration=%buildConfig%
 if errorlevel 1 goto BuildFail
 goto BuildSuccess
