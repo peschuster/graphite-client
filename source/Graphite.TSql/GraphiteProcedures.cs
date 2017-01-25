cfd70ca40adbf51e715dd57aaf19221114a256a3
sroblin <sebastien.roblin@gmail.com>
2017-01-25 15:44:28 +0100

    Update GraphiteProcedures.cs

--- source/Graphite.TSql/GraphiteProcedures.cs
+++ source/Graphite.TSql/GraphiteProcedures.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net;
+using System.Data;
+using System.IO;
 using Microsoft.SqlServer.Server;
 
 namespace Graphite.TSql
@@ -20,6 +22,34 @@ namespace Graphite.TSql
                 catch (InvalidOperationException exception)
                 {
                     SqlContext.Pipe.Send(exception.Message);
+                }
+            }
+        }
+        
+        [SqlProcedure]
+        public static void GraphiteSendSeries(string host, int port, string Series, out string returnString)
+        {
+            IPAddress address = Helpers.ParseAddress(host);
+
+            using (var pipe = new TcpPipe(address, port))
+            {
+                returnString = "";
+                try
+                {
+                    DataSet ds = new DataSet();
+                    ds.ReadXml(new StringReader(Series));
+                    //DataTable dt = ds.Tables[0];
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        pipe.Send(GraphiteFormatter.Format(dr[0].ToString(), Convert.ToInt32(dr[1].ToString())));
+
+                    }
+                    returnString = ds.Tables[0].Rows.Count + " values sent";   
+                }
+                catch (InvalidOperationException exception)
+                {
+                    SqlContext.Pipe.Send(exception.Message);
+                    returnString = exception.Message;
                 }
             }
         }
